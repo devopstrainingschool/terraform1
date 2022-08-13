98a984c13e42631f95e3f5bcfbcf72191bc1182d
yannick <yannickparker84@gmail.com>
2022-08-12 21:08:02 -0500

    jhjj

--- variable.tf
+++ variable.tf
@@ -4,8 +4,7 @@ variable "region" {
 
 variable "aws_centos_amis" {
     default = {
-        "us-east-2" = "CentOS-7-2111-20220330_2.x86_64-d9a3032a-921c-4c6d-b150-bde168105e42
-ami-0a5588cee1fe39fff"
+        "us-east-2" = "ami-0a5588cee1fe39fff"
     }
 }
 

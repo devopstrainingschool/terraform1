88d2d85a9f9ea89f1cb76f694437a1d59220f899
yannick <yannickparker84@gmail.com>
2022-08-12 21:28:48 -0500

    jhjj

--- lb.tf
+++ lb.tf
@@ -1,27 +1,32 @@
-resource "aws_elb" "lb" {
-    name_prefix = "${var.environment}-"
-    subnets = ["${aws_subnet.webserver.id}"]
-    health_check {
-        healthy_threshold = 2
-        unhealthy_threshold = 2
-        timeout = 3
-        target = "TCP:80"
-        interval = 30
-    }
-    listener {
-        instance_port = 80
-        instance_protocol = "http"
-        lb_port = 80
-        lb_protocol = "http"
-    }
-    cross_zone_load_balancing = true
-    instances = ["${aws_instance.webserver.id}"]
-    security_groups = ["${aws_security_group.LoadBalancerSG.id}"]
-}
- 
- resource "aws_lb_cookie_stickiness_policy" "gitlab" {
-  name                     = "gitlab-policy"
-  load_balancer            = "${aws_elb.lb.id}"
-  lb_port                  = 80
-  cookie_expiration_period = 600
+
+resource "aws_elb" "elb" {
+  name               = "terraform-elb"
+  availability_zones = ["us-east-2a"]
+  
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8000/"
+    interval            = 30
+  }
+
+  instances                   = [aws_instance.webserver.id]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "foobar-terraform-elb"
+  }
 }

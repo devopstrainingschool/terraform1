3e444755408aeb152c20f494a0c7bf9bfb03fe48
yannick <yannickparker84@gmail.com>
2022-08-12 22:28:24 -0500

    jhjj

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket  = "terraform-remote-state-s3"
+    key     = "terraform-infrastructure.tfstate"
+    region  = "us-west-2"
+    encrypt = "true"
+    dynamodb_table = "hands-on-cloud-terraform-remote-state-dynamodb"
+  }
+}

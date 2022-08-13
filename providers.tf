65ef554a2cad42226d21b42ef4160741f23f0607
yannick <yannickparker84@gmail.com>
2022-08-12 20:14:15 -0500

    jhjj

--- providers.tf
+++ providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     ansible = {
       source = "nbering/ansible"
-      version = "=>1.0.4"
+      version = ">=1.0.4"
     }
     aws = {
       source = "hashicorp/aws"
-      version = "=>3.42.0"
+      version = ">=3.42.0"
     }
   }
 }

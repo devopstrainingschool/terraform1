df2258a188c678600cbed362b556ec528f0ca028
yannick <yannickparker84@gmail.com>
2022-08-12 21:52:56 -0500

    jhjj

--- lb.tf
+++ lb.tf
@@ -3,8 +3,8 @@ resource "aws_lb" "test" {
   name               = "test-lb-tf"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.LoadBalancerSG.id]
-  subnets            = [aws_subnet.webserver.id]
+  security_groups    = ["${aws_security_group.LoadBalancerSG.id}"]
+  subnets            = ["${aws_subnet.webserver.id}"]
   
 
 

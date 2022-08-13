79fe8a5afd91c40dc9b1d385b7b6bc349684e43a
yannick <yannickparker84@gmail.com>
2022-08-12 21:53:46 -0500

    jhjj

--- lb.tf
+++ lb.tf
@@ -4,7 +4,7 @@ resource "aws_lb" "test" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = ["${aws_security_group.LoadBalancerSG.id}"]
-  subnets            = ["${aws_subnet.webserver.id}"]
+  subnets            = ["${aws_subnet.webserver.id}","${aws_subnet.database.id}"]
   
 
 

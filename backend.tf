8a7c25002e1c9ee5d56d0551d2dc351d5ba1bd11
yannick <yannickparker84@gmail.com>
2022-08-15 10:05:58 -0500

    ksk

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "terraform-remote-state-s3"
     key     = "terraform-infrastructure.tfstate"
-    region  = "us-west-2"
+    region  = "us-east-2"
     encrypt = "true"
     dynamodb_table = "hands-on-cloud-terraform-remote-state-dynamodb"
   }
